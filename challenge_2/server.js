93da31ffdc29e293b3218aed1191b0ef00d47722
Michael Tobias <michaelremitobias@gmail.com>
2020-09-19 12:10:48 -0700

    Successfully adding to server from form

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+app = express();
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('client'));
 
+app.post('/submit-form', (req, res) => {
+  const data = req.body.csvdata
+  res.end(console.log(data))
+})
 
+app.listen(5000, () => {
+  console.log('Listening on post 5000...')
+});
