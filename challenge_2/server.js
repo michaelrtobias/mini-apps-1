20b17812d88220b1d1b3ebfc464e81e1a11a293f
Michael Tobias <michaelremitobias@gmail.com>
2020-09-19 17:35:08 -0700

    End of the day push

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const path = require('path');
 app = express();
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static('client'));
+app.use(express.json())
+
+
 
 app.post('/submit-form', (req, res) => {
-  const data = req.body.csvdata
-  res.end(console.log(data))
+  var csv = req.body.csvdata;
+  console.log(csv)
+  res.end(csv + ' submitted correctly' + JSON.stringify(req.body))
 })
+
+
 
 app.listen(5000, () => {
   console.log('Listening on post 5000...')
